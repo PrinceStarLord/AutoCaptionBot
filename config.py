7540e85f67e5d598a390f77bfa76495caf526834
Tushar Padir <86295965+PrinceStarLord@users.noreply.github.com>
2025-04-03 19:18:35 +0530

    Using {file_caption}

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 API_ID = int(os.environ.get("API_ID", "523894"))
 API_HASH = os.environ.get("API_HASH", "e15ad5314295f")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "683894700450:AAGtmIuSJgmjbkbi3vs")
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_name}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>")
