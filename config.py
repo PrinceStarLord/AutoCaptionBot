27109ae27703e693bf3975c57fe32ea83a1dd321
Tushar Padir <86295965+PrinceStarLord@users.noreply.github.com>
2025-12-30 19:56:31 +0530

    ADMINS variable Fixed
    Refactor CUSTOM_CAPTION assignment and convert ADMINS to a list of integers.

--- config.py
+++ config.py
@@ -3,5 +3,10 @@ import os
 API_ID = int(os.environ.get("API_ID", "523894"))
 API_HASH = os.environ.get("API_HASH", "e15ad5314295f")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "683894700450:AAGtmIuSJgmjbkbi3vs")
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>")
-ADMINS = os.environ.get("ADMINS", "6167872503")
+CUSTOM_CAPTION = os.environ.get(
+    "CUSTOM_CAPTION",
+    "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>"
+)
+
+# ✅ MUST be a list[int]
+ADMINS = list(map(int, os.environ.get("ADMINS", "6167872503").split()))
