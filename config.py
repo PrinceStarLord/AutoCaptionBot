8b1eeb6ec40055878903c123ebd5e0f65bc2adad
Tushar Padir <86295965+PrinceStarLord@users.noreply.github.com>
2025-12-30 22:57:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ API_HASH = os.environ.get("API_HASH", "e15ad5314295f")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "683894700450:AAGtmIuSJgmjbkbi3vs")
 CUSTOM_CAPTION = os.environ.get(
     "CUSTOM_CAPTION",
-    "<b>{file_caption}</b>\n\n<b>Audio : {languages}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>"
+    "<b>{file_caption}</b>\n\n<b>JOIN 💎 : @M2LINKS</b>"
 )
 ADMINS = list(map(int, os.environ.get("ADMINS", "6167872503").split()))
